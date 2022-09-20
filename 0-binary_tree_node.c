f8080dee8428f3e24074925f646fd70a290a2965
Astewul <loloalex1992@gmail.com>
2022-09-20 15:22:42 -0300

    Task completed

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,12 +1,10 @@
 #include "binary_trees.h"
-
 /**
  * binary_tree_node
  * @parent: node parent
  * @value: value
  * Return: return new node 
  **/
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t  *new_node;
