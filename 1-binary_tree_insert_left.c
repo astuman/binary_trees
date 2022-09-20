56d041c6e11b7cb75a4429dbc02f9be7678d8ac4
Astewul <loloalex1992@gmail.com>
2022-09-20 16:12:12 -0300

    task two completed

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,10 +15,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value){
 		return (NULL);
 	}
 	new_node = binary_tree_node(parent, value);
-		if(new_node == NULL)
-		{
-			return (NULL);
-		}
+	if(new_node == NULL)
+	{
+	return (NULL);
+	}
 	if (parent->left != NULL)
 	{
 		new_node->left = parent->left;
