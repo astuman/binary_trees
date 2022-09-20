4f2092790506bdffa9b836d19e2f0897e556ed22
Astewul <loloalex1992@gmail.com>
2022-09-20 05:21:57 -0300

    commit first task

--- binary_trees.h
+++ binary_trees.h
@@ -8,7 +8,9 @@
 #include <limits.h>
 
 /**
- * struct binary_tree_s - Binary tree node
+ * @brief 
+ * 
+ */
  *
  * @n: Integer stored in the node
  * @parent: Pointer to the parent node
@@ -34,9 +36,10 @@ typedef struct link_s
 	size_t n;
 	struct binary_tree_s const *node;
 	struct link_s *next;
-} link_t;
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+}link_t;
+void binary_tree_print(const binary_tree_t *)
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
@@ -55,3 +58,4 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
+#endif
