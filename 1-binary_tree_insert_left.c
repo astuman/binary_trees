d6dcdede988ef28aac3d327e2f56b252f0dc649c
Astewul <loloalex1992@gmail.com>
2022-09-21 13:05:46 -0300

    task completed

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,7 +2,6 @@
 
 /**
  * binary_tree_insert_left - add a node in the left of the parent
- * if it exists it move down one level and add the new node first
  * @parent: parent of the specified node
  * @value: value of the node
  * Return: NULL if it fails or the new node
