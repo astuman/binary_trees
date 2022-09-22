47f9561d5dd2a8142843f1a7d737c071563f624f
Adonias-sahile <102035986+Adonias-sahile@users.noreply.github.com>
2022-09-22 12:14:05 -0700

    Update 7-binary_tree_inorder.c

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 /**
- * binary_tree_inorder - print using in-order traversal
- * @tree: tree
+ * binary_tree_inorder - print elements of tree using in-order traversal
+ * @tree: tree to go through
  * @func: function to use
  * Return: Nothing
  */
